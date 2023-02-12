d6134ee1ef2e95fe769bf62c8bca5e0c6961448c
Ali Raza <arazxa@gmail.com>
2023-02-12 20:33:35 +0500

    update 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AmaDetailsComponent } from "./ama-details/ama-details.component";
 import { AMAComponent } from "./ama/ama.component";
+import { COMINGSOONComponent } from "./coming-soon/coming-soon.component";
 import { HomeComponent } from "./home/home.component";
 import { NewsDetailsComponent } from "./news-details/news-details.component";
 import { NewsComponent } from "./news/news.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
+  { path: "coming-soon", component: COMINGSOONComponent },
   { path: "news", component: NewsComponent },
   { path: "ama", component: AMAComponent },
   { path: "ama-details", component: AmaDetailsComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NewsComponent } from './news/news.component';
-import { AMAComponent } from './ama/ama.component';
-import { AmaCardComponent } from './ama-card/ama-card.component';
-import { AmaDetailsComponent } from './ama-details/ama-details.component';
-import { NewsDetailsComponent } from './news-details/news-details.component';
-import { FaqComponent } from './faq/faq.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { COMINGSOONComponent } from "./coming-soon/coming-soon.component";
+import { NewsComponent } from "./news/news.component";
+import { AMAComponent } from "./ama/ama.component";
+import { AmaCardComponent } from "./ama-card/ama-card.component";
+import { AmaDetailsComponent } from "./ama-details/ama-details.component";
+import { NewsDetailsComponent } from "./news-details/news-details.component";
+import { FaqComponent } from "./faq/faq.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NewsComponent, AMAComponent, AmaCardComponent, AmaDetailsComponent, NewsDetailsComponent, FaqComponent],
+  declarations: [
+    AppComponent,
+    COMINGSOONComponent,
+    HomeComponent,
+    NewsComponent,
+    AMAComponent,
+    AmaCardComponent,
+    AmaDetailsComponent,
+    NewsDetailsComponent,
+    FaqComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
